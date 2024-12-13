d9dde93e00e8f5cf41f95acbaef1231c3c3084ca
Kittipong <anonymous51124@gamil.com>
2024-12-13 16:52:13 +0700

    lab 9 2 complete

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,24 +1,42 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int row, int colum)
+{
+	if (row <= 0 or colum <= 0)
+	{
+		cout << "Invalid input";
+	}
+	else
+	{
+		for (int u = row; u > 0; --u)
+		{
+			for (int i = colum; i > 0; --i)
+			{
+				cout << "O";
+			}
+			cout << endl;
+		}
+	}
+}
 
-int main(){
-	
-	printO(2,2);
+int main()
+{
+
+	printO(2, 2);
 	cout << "\n";
-	
+
 	printO(3,5);
-	cout << "\n";	
-	
+	cout << "\n";
+
 	printO(5,3);
-	cout << "\n";	
-	
+	cout << "\n";
+
 	printO(0,3);
-	cout << "\n";	
-	
+	cout << "\n";
+
 	printO(7,-1);
-	cout << "\n";	
-	
+	cout << "\n";
+
 	return 0;
 }
